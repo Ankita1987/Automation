b2df1905919ebe23f8db9de54cc1ade79da2e43c
Ankita1987 <ankitarawat6@gmail.com>
2019-11-17 19:40:43 +0530

    Updating

--- src/Practise/ArmstrongNumber.java
+++ src/Practise/ArmstrongNumber.java
@@ -4,6 +4,7 @@ public class ArmstrongNumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Updating in GIT
 		Armstrong();
 	}
 public static void Armstrong()
